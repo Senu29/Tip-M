69d224c63418500eb6db39bf5c52c5d471873c67
Senuda Ratnayake <tdr5250@psu.edu>
2022-09-27 14:05:20 -0400

    Initial Commit

---
+++ Tip'M/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tip'M
+//
+//  Created by Senuda Ratnayake on 9/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
